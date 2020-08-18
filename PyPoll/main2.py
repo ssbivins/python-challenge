47e707cc0dc403726e9d51b4d86cd30bb84e7cbe
Susan Bivins <ssbivins@gmail.com>
2020-08-18 09:31:03 -0500

    Working except for rounding percent of votes

--- PyPoll/main2.py
+++ PyPoll/main2.py
@@ -28,15 +28,11 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         # Get the row[2] to get the candidate's name
         cand = row[2]
-        if (cand == "" or cand == " "):
-            break
         # If the name is not in the dictionary, add it with a value of 0
         if cand not in canVotes:
             canVotes[cand] = 0
         # Add 1 to  the name's counter
         canVotes[cand] += 1
-        # Add 1 to total votes
-        totalVotes += 1
     print(canVotes)
 
     # Print out results 
@@ -46,19 +42,22 @@ with open(csvpath) as csvfile:
     print("--------------------")
     
     # Go through dictionary canVotes to get and print results for each candidate 
-    #for k,v in canVotes:
-    for c in canVotes:
-        name = c
-        votes = canVotes[c]
-        calcPercentVotes = (votes/totalVotes*100)
-        percentVotes = calcPercentVotes(round,3) 
+    for k,v in canVotes:
+        name = k
+        votes = int(v)
+        percentVotes = (v/totalVotes*100)
+        print(f'{name} {votes} {percentVotes}%') 
         # Print candidate information
-        print(f'{name}: {percentVotes}% {votes}')
+        print(f'{name}:  {percentVotes}% ({votes})'
         # Get the highest number of votes to determine winner
-        if (votes > votesSoFar):
-            votesSoFar = votes
-            winnerSoFar = name
+        #if (votes > votesSoFar):
+        #    votesSoFar = votes
+        #    winnerSoFar = k
     print("--------------------")
-    print(f'Winner: {winnerSoFar}')
+    print(f 'Winner: ' {winnerSoFar})
     print("--------------------")   
 
+    print(f'Total: ${total}')
+    print(f'Average Change: ${averageChange}')
+    print(f'Greatest Increase in Profits: {increaseMonth} ${increaseSoFar}')
+    print(f'Greatest Decrease in Profits: {decreaseMonth} ${decreaseSoFar}')
